2234336a7635c36e612c9a6d005218ce8a235fba
James Nguyen <ndhoang7089@gmail.com>
2024-05-20 14:50:01 +0700

    first commit

---
+++ src/main/java/test/Dummy/API/demo/TestDummyApiApplication.java
@@ -0,0 +1,13 @@
+package test.Dummy.API.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDummyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDummyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/Dummy/API/demo/TestDummyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package test.Dummy.API.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDummyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
